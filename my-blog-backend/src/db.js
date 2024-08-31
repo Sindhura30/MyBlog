b31ba38004f227a3fa1120a2c3c42b90ea28196f
Sindhura Shetty <sindhura.shetty@lowes.com>
2024-08-31 19:30:51 +0530

    Initial commit

---
+++ my-blog-backend/src/db.js
@@ -0,0 +1,11 @@
+import { MongoClient } from "mongodb";
+let db;
+
+const connectToDb = async (cb) => {
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.8bbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await client.connect();
+    db = client.db('react-blog-db');
+    cb();
+}
+
+export {db, connectToDb };
